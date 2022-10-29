e7f680046d8c794697eb48f06e2138104a7a230e
GustavodspHub <77053633+GustavodspHub@users.noreply.github.com>
2022-10-28 21:10:22 -0300

    refactor: change environments

--- src/utils/constants/environment.ts
+++ src/utils/constants/environment.ts
@@ -9,11 +9,3 @@ export const APIS = {
   HUBSPOT_BASE_URL: process.env.HUBSPOT_BASE_URL || '',
   GOOGLE_BASE_URL: process.env.GOOGLE_BASE_URL || ''
 }
-
-export const ID = {
-  SHEET_ID: process.env.SHEET_ID || ''
-}
-
-export const TOKEN = {
-  ACCESS_TOKEN_HUBSPOT: process.env.ACCESS_TOKEN_HUBSPOT || 'pat-na1-b4a7bd63-825d-4d1c-86b8-d18b893cd51b'
-}
